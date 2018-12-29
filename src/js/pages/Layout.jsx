2b7fd5ac7ace2c4c024852549760eb7eb5a860fd
Jason Boyll <jason.boyll@gmail.com>
2018-12-29 00:34:08 -0500

    Add bulma css to project

--- src/js/pages/Layout.jsx
+++ src/js/pages/Layout.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import "bulma/css/bulma";
 
 export default class Layout extends Component {
   render() {
     return(
-      <div>
-        <h1>Hello, World!</h1>
+      <div className="section is-paddingless">
+        <div className="hero has-background-dark is-full-width">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title has-text-white">Keep the change</h1>
+              <h2 className="subtitle has-text-white">Ya filthy animal</h2>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
